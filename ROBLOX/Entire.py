9bc7f3b62f5971c304f0e2c4cf44d138bf009a50
Guzmania <61605350+MaulDandHaxSurviv@users.noreply.github.com>
2021-05-20 10:53:29 -0400

    Add files via upload

---
+++ ROBLOX/Entire.py
@@ -0,0 +1,3 @@
+# This is the entire server script. DONT DELETE
+    
+print "IfDeleteTrue=DoAction;EndProject\n"
---
+++ ROBLOX/main.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                              C++ for my website.
+               Compile test for this repository.
+Dont delete this file because if you do the entire thing gets delted.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"RunEntireProject=true";
+
+    return 0;
+}
